beeb83132d93aaa11b4b37d55bb9a038882cdc9f
steff880 <stefan.d.yordanov@gmail.com>
2021-01-31 01:07:22 +0000

    Minor fixes

--- assets/js/book-now.js
+++ assets/js/book-now.js
@@ -15,14 +15,14 @@ function validation() {
         textMessage.innerHTML = "Email Address is Valid";
         textMessage.style.color = "#00ff00";
         textMessage.style.display = "block";
-        textMessage.style.padding = "0px 10px 5px 5px"
+        textMessage.style.padding = "0px 10px 5px 5px";
     } else {
         form.classList.remove("valid");
         form.classList.add("invalid");
         textMessage.innerHTML = "Please Enter Valid Email Address ";
-        textMessage.style.color = "#ff0000"
+        textMessage.style.color = "#ff0000";
         textMessage.style.display = "block";
-        textMessage.style.padding = "0px 10px 5px 5px"
+        textMessage.style.padding = "0px 10px 5px 5px";
     }
 
     if (email === "") {
@@ -31,7 +31,7 @@ function validation() {
         textMessage.innerHTML = "";
         textMessage.style.color = "#00ff00";
         textMessage.style.display = "none";
-        textMessage.style.padding = "0"
+        textMessage.style.padding = "0";
     }
 }
 
@@ -39,15 +39,15 @@ function validation() {
 
 // Submit 
 
-document.getElementById('form-bookNow').addEventListener('submit', function (e) {
+document.getElementById("form-bookNow").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let form = document.getElementById('form-bookNow');
-    let alertMessage = document.getElementById('alert-message');
-    let btnReset = document.getElementById('btnReset');
+    let form = document.getElementById("form-bookNow");
+    let alertMessage = document.getElementById("alert-message");
+    let btnReset = document.getElementById("btnReset");
 
     if (form.checkValidity()) {
-        alertMessage.classList.add('alert', 'alert-success');
+        alertMessage.classList.add("alert", "alert-success");
         alertMessage.innerHTML = "Your Booking is Successful";
         alertMessage.style.display = "block";
         btnReset.classList.remove("btn-reset-hide");
@@ -58,7 +58,7 @@ document.getElementById('form-bookNow').addEventListener('submit', function (e) 
 
 
     setTimeout(function () {
-        alertMessage.classList.remove("alert", "alert-success")
+        alertMessage.classList.remove("alert", "alert-success");
         alertMessage.innerHTML = "";
     }, 3000);
 
